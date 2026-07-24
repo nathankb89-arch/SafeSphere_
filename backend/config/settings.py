98f8c4d4100f5a9a24771a43c6af63524003d950
nate <nathankb89@gmail.com>
2026-07-24 12:35:20 +0300

    Update settings.py to use environment variables for SECRET_KEY, DEBUG, and database configuration

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p9)keffqyx=zo)h_%!b=3832ev7=wu!%ys6g3s^we1$kajo4^h'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -87,13 +88,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': {
+'ENGINE':'django.db.backends.postgresql',
+'NAME':config('DB_NAME'),
+'USER':config('DB_USER'),
+'PASSWORD':config('DB_PASSWORD'),
+'HOST':config('DB_HOST'),
+'PORT':config('DB_PORT'),
 }
 
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
