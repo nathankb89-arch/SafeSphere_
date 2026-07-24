8136de4f8ecd8f6cbb61121115d0cf885352c050
nate <nathankb89@gmail.com>
2026-07-24 10:12:06 +0300

    Fix middleware order in settings.py to ensure proper CORS handling

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -59,6 +59,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+
+    'django.middleware.security.SecurityMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
